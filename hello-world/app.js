0823a8c08811f40f466a488cbb0497d18594526e
pankesh.patel <pankesh.patel@bolt-data.com>
2025-02-20 22:09:30 -0500

    added architecture component

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,19 +1,5 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
-let response;
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
+let response;
 exports.lambdaHandler = async (event, context) => {
     try {
         // const ret = await axios(url);
@@ -21,7 +7,6 @@ exports.lambdaHandler = async (event, context) => {
             'statusCode': 200,
             'body': JSON.stringify({
                 message: 'hello world',
-                // location: ret.data.trim()
             })
         }
     } catch (err) {
